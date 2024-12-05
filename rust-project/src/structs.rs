2bf2a36e3a6a6361d4238fd4007768c93c6046b6
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2024-12-05 11:24:29 +0530

    implementation of structs

--- rust-project/src/structs.rs
+++ rust-project/src/structs.rs
@@ -4,6 +4,21 @@ struct User {
     age: u32, //unsigned 32 bit integer
     active: bool,
 }
+//implementing structs
+struct Square {
+    len: u32,
+    breadth: u32,
+}
+impl Square {
+    fn area(&self) -> u32 { //self is the 1st attribute
+        return self.len*self.breadth;
+    }
+    //can have multiple implementations
+    fn peri(&self) -> u32 {
+        return 2*(self.len + self.breadth);
+    }
+}
+
 fn main() {
     let name = String::from("AdSiNcE2k4");
     let user = User {
@@ -12,5 +27,12 @@ fn main() {
         active: true,
     };
     println!("{} is {} years old ", user.name, user.age);
+
+    let square = Square {
+        len: 1,
+        breadth: 7,
+    };
+    println!("The area is {}", square.area());
+    println!("The perimeter is {}", square.peri());
 }
 //tuple struct, unit struct
